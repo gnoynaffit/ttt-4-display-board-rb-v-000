11c866401fee6567b7e9c6e5d563060bf13cc338
Tiffany <ong.tifffany1503@gmail.com>
2018-02-03 21:08:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  puts "Row 2: #{X{4}}"
+
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
