401e9ee73130bff2653aadfc532e7b962db731b9
Tiffany <ong.tifffany1503@gmail.com>
2018-02-03 21:40:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,13 +2,13 @@
 # out the current state.
 def display_board(board)
 
-  puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
-  puts "----------------------------------"
-  puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
-  puts "----------------------------------"
-  puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
+  puts "   |   |  "
+  puts "----------"
+  puts "   |   |  "
+  puts "----------"
+  puts "   |   |  "
 
-  end
+end
 puts "Turn 1"
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
   display_board(board)
