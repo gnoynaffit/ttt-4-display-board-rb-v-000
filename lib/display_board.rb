a6c9e746b3177e0b9d841c23cc36c8db7eb608a2
Tiffany <ong.tifffany1503@gmail.com>
2018-02-03 22:22:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,4 +9,4 @@ def display_board(board)
   puts "   |   |   "
 end
 
-puts = display_board([" ", " ", " ", " ", " ", " #{board[4]} "," ", " ", " ", " ", ])
+puts = board([" ", " ", " ", " ", " ", " #{board[4]} "," ", " ", " ", " ", ])
